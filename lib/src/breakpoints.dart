bb0b93c4f76603a8fe9c0cd2957b481b333a421b
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2021-07-30 21:58:54 +0200

    Update breakpoints

--- lib/src/breakpoints.dart
+++ lib/src/breakpoints.dart
@@ -1,14 +1,14 @@
 class BreakPoint {
-  static const mobileSmall = 320.0;
-  static const mobileNormal = 375.0;
-  static const mobileLarge = 414.0;
-  static const mobileExtraLarge = 480.0;
-  static const tabletSmall = 600.0;
-  static const tabletNormal = 768.0;
-  static const tabletLarge = 840.0;
-  static const tabletExtraLarge = 1080.0;
-  static const desktopSmall = 1280.0;
-  static const desktopNormal = 1775.0;
+  static const mobileSmall = 375.0;
+  static const mobileNormal = 410.0;
+  static const mobileLarge = 450.0;
+  static const mobileExtraLarge = 550.0;
+  static const tabletSmall = 650.0;
+  static const tabletNormal = 780.0;
+  static const tabletLarge = 880.0;
+  static const tabletExtraLarge = 1180.0;
+  static const desktopSmall = 1270.0;
+  static const desktopNormal = 1780.0;
   static const desktopLarge = 3540.0;
   static const desktopExtraLarge = 4096.0;
 }

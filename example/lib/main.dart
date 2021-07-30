a096fdabca49fd57f01ce358a30b16055023c0dd
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2021-07-30 22:21:12 +0200

    Add new example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,10 +21,11 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _buildExampleOne(context);
+    return _buildExample2(context);
+    //_buildExample1(context);
   }
 
-  Widget _buildExampleOne(BuildContext context) {
+  Widget _buildExample1(BuildContext context) {
     return Scaffold(
       body: SizeHelper.of(context, printScreenInfo: true).helpBuilder(
         mobileNormal: (width, height) => Container(
@@ -72,6 +73,58 @@ class Home extends StatelessWidget {
           color: height > 850 ? Colors.orange : Colors.yellow,
           child: Text('desktopNormal'),
         ),
+      ),
+    );
+  }
+
+  Widget _buildExample2(BuildContext context) {
+    final dataList = List.generate(15, (index) => 'Card ${index + 1}');
+    final cardsList = dataList.map((e) => CardWidget(text: e)).toList();
+    return Scaffold(
+      body: context.sizeHelperBuilder(
+        mobileExtraLarge: (_, __) => SingleChildScrollView(
+            child: Column(
+          children: cardsList,
+        )),
+        tabletLarge: (_, __) => SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: cardsList,
+            )),
+        desktopSmall: (_, __) => Wrap(
+          children: cardsList,
+        ),
+      ),
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  const CardWidget({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 250,
+      height: 200,
+      margin: const EdgeInsets.all(16.0),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        color: Colors.grey[200],
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 3),
+            blurRadius: 6,
+          ),
+        ],
+      ),
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 24.0, color: Colors.green),
       ),
     );
   }

76f3904dbfdbd7333a91b34673e4b64805daecc6
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2023-06-13 15:30:36 +0300

    New BreakPoints

--- lib/src/breakpoints.dart
+++ lib/src/breakpoints.dart
@@ -4,18 +4,18 @@ class BreakPoint {
   final String name;
   final double value;
 
-  static const mobileSmall = BreakPoint._('mobileSmall', 375.0);
-  static const mobileNormal = BreakPoint._('mobileNormal', 410.0);
-  static const mobileLarge = BreakPoint._('mobileLarge', 450.0);
-  static const mobileExtraLarge = BreakPoint._('mobileExtraLarge', 550.0);
-  static const tabletSmall = BreakPoint._('tabletSmall', 650.0);
-  static const tabletNormal = BreakPoint._('tabletNormal', 780.0);
-  static const tabletLarge = BreakPoint._('tabletLarge', 880.0);
-  static const tabletExtraLarge = BreakPoint._('tabletExtraLarge', 1180.0);
-  static const desktopSmall = BreakPoint._('desktopSmall', 1270.0);
-  static const desktopNormal = BreakPoint._('desktopNormal', 1780.0);
-  static const desktopLarge = BreakPoint._('desktopLarge', 3540.0);
-  static const desktopExtraLarge = BreakPoint._('desktopExtraLarge', 4096.0);
+  static const mobileSmall = BreakPoint._('mobileSmall', 275.0);
+  static const mobileNormal = BreakPoint._('mobileNormal', 325.0);
+  static const mobileLarge = BreakPoint._('mobileLarge', 350.0);
+  static const mobileExtraLarge = BreakPoint._('mobileExtraLarge', 375.0);
+  static const tabletSmall = BreakPoint._('tabletSmall', 400.0);
+  static const tabletNormal = BreakPoint._('tabletNormal', 400.0);
+  static const tabletLarge = BreakPoint._('tabletLarge', 450.0);
+  static const tabletExtraLarge = BreakPoint._('tabletExtraLarge', 600.0);
+  static const desktopSmall = BreakPoint._('desktopSmall', 950.0);
+  static const desktopNormal = BreakPoint._('desktopNormal', 1150.0);
+  static const desktopLarge = BreakPoint._('desktopLarge', 1500.0);
+  static const desktopExtraLarge = BreakPoint._('desktopExtraLarge', 1800.0);
 
   static List<BreakPoint> get values => [
         mobileSmall,
